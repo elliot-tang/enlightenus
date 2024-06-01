1b419c0aa4f28be8664797fe2417bcee32c4a6df
Elliot Tang <128604750+elliot-tang@users.noreply.github.com>
2024-06-01 23:01:48 +0800

    Update user.js

--- schema/user.js
+++ schema/user.js
@@ -10,26 +10,3 @@ const User = mongoose.model('User', userSchema);
 module.exports = User;
 
 module.exports = User;
-
-// Test Case
-// run().catch(err => console.log(err));
-
-// async function run() {
-//   await mongoose.connect('mongodb+srv://elliottang379:K09aaUaomW38T3SW@m0cluster.i2lyqzv.mongodb.net/enlightenus?retryWrites=true&w=majority&appName=M0Cluster');
-
-//   const user = new User({
-//     email: 'e1156752@u.nus.edu',
-//     username: 'elliot-tangy',
-//     password: 'password2',
-//   });
-
-//   try {
-//     await user.save();
-//   } catch (error) {
-//     console.error(error);
-//   }
-//   await user.save();
-
-//   console.log(user.email); 
-// }
-
